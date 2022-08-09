49612788c55b2ec68fc50695deddaa1a38b273b0
ogzgul <oguz_gul216@hotmail.com>
2022-08-09 14:30:35 +0300

    Ödev Yapıldı

---
+++ script.js
@@ -0,0 +1,32 @@
+// isim kısmı
+let name = prompt(`Lütfen isminizi giriniz`);
+let myName = document.querySelector(`#myName`);
+if (name) {
+  myName.innerHTML = `${name}`;
+} else {
+  alert(`Lütfen isim kısmını boş bırakmayınız`);
+  location.reload();
+}
+
+// Saat ve gün kısmı
+let days = [
+  "Pazar",
+  "Pazartesi",
+  "Salı",
+  "Çarşamba",
+  "Perşembe",
+  "Cuma",
+  "Cumartesi",
+];
+function clockFunction() {
+  let dateTime = new Date();
+  let day = days[dateTime.getDay()];
+  let hrs = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+  document.querySelector(
+    "#myClock"
+  ).innerHTML = `${hrs}: ${min}: ${sec}  ${day}`;
+}
+
+setInterval(clockFunction, 1000);
